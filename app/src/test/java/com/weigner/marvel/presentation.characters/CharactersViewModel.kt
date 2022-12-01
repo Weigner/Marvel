f66f9d4537214bd681bce8e9c640476148bb789c
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2022-12-01 08:28:50 -0300

    Feito teste unitario da classe CharactersViewModel.kt

---
+++ app/src/test/java/com/weigner/marvel/presentation.characters/CharactersViewModel.kt
@@ -0,0 +1,77 @@
+package com.weigner.marvel.presentation.characters
+
+import androidx.paging.PagingData
+import com.nhaarman.mockitokotlin2.any
+import com.nhaarman.mockitokotlin2.whenever
+import com.weigner.core.domain.model.Character
+import com.weigner.core.usecase.GetCharactersUseCase
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import kotlinx.coroutines.flow.count
+import kotlinx.coroutines.flow.flowOf
+import kotlinx.coroutines.test.TestCoroutineDispatcher
+import kotlinx.coroutines.test.runBlockingTest
+import kotlinx.coroutines.test.setMain
+import org.junit.Assert.assertEquals
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.mockito.Mock
+import org.mockito.junit.MockitoJUnitRunner
+import java.lang.RuntimeException
+
+@RunWith(MockitoJUnitRunner::class)
+class CharactersViewModelTest {
+
+    @ExperimentalCoroutinesApi
+    val testDispatcher: TestCoroutineDispatcher = TestCoroutineDispatcher()
+
+    @Mock
+    lateinit var getCharactersUseCase: GetCharactersUseCase
+
+    private lateinit var charactersViewModel: CharactersViewModel
+
+    private val pagingDataCharacters = PagingData.from(
+        listOf(
+            Character(
+                "Spider Man",
+                "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+            ),
+            Character(
+                "Spider Man",
+                "https://i.annihil.us/u/prod/marvel/i/mg/c/e0/535fecbbb9784.jpg"
+            )
+        )
+    )
+
+    @Before
+    fun setUp() {
+        Dispatchers.setMain(testDispatcher)
+        charactersViewModel = CharactersViewModel(getCharactersUseCase)
+    }
+
+    @ExperimentalCoroutinesApi
+    @Test
+    fun `should alidate the paging data object values when calling charactersPagingData`() =
+        runBlockingTest {
+            whenever(
+                getCharactersUseCase.invoke(any())
+            ).thenReturn(
+                flowOf(pagingDataCharacters)
+            )
+
+            val result = charactersViewModel.charactersPagingData("")
+
+            assertEquals(1, result.count())
+        }
+
+    @ExperimentalCoroutinesApi
+    @Test(expected = RuntimeException::class)
+    fun `should throw an exception when the calling to the use case returns an exception`() =
+        runBlockingTest {
+            whenever(getCharactersUseCase.invoke(any())).thenThrow(RuntimeException())
+
+            charactersViewModel.charactersPagingData("")
+        }
+
+}
--- core/src/main/java/com/weigner/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/com/weigner/core/usecase/GetCharactersUseCase.kt
@@ -5,13 +5,23 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.weigner.core.data.repository.CharactersRepository
 import com.weigner.core.domain.model.Character
+import com.weigner.core.usecase.GetCharactersUseCase.GetCharactersParams
 import com.weigner.core.usecase.base.PagingUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-class GetCharactersUseCase @Inject constructor(
+interface GetCharactersUseCase {
+
+    operator fun invoke(params: GetCharactersParams): Flow<PagingData<Character>>
+
+    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
+
+}
+
+class GetCharactersUseCaseImpl @Inject constructor(
     private val charactersRepository: CharactersRepository
-) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
+) : PagingUseCase<GetCharactersParams, Character>(),
+    GetCharactersUseCase {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
         return Pager(config = params.pagingConfig) {
@@ -19,5 +29,4 @@ class GetCharactersUseCase @Inject constructor(
         }.flow
     }
 
-    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
 }
