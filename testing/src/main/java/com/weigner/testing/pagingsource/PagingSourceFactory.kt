9b5cfe6eb0546de191d25c311fe09c87bd731210
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2023-07-23 15:53:17 -0300

    Implementa testes unitarios

---
+++ testing/src/main/java/com/weigner/testing/pagingsource/PagingSourceFactory.kt
@@ -0,0 +1,21 @@
+package com.weigner.testing.pagingsource
+
+import androidx.paging.PagingSource
+import androidx.paging.PagingState
+import com.weigner.core.domain.model.Character
+
+class PagingSourceFactory {
+
+    fun create(heroes: List<Character>) = object: PagingSource<Int, Character>() {
+        override fun getRefreshKey(state: PagingState<Int, Character>) = 1
+
+        override suspend fun load(params: LoadParams<Int>): LoadResult<Int, Character> {
+            return LoadResult.Page(
+                data = heroes,
+                prevKey = null,
+                nextKey = 20
+            )
+        }
+
+    }
+}
