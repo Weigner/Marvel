3c3bbfe5839b043e3cbb4720dd81f262c52c17e0
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2023-10-22 16:10:31 -0300

    Resolve erro no hilt

--- app/src/main/java/com/weigner/marvel/framework/di/UseCaseModule.kt
+++ app/src/main/java/com/weigner/marvel/framework/di/UseCaseModule.kt
@@ -5,9 +5,10 @@ import com.weigner.core.usecase.GetComicsUseCaseImpl
 import dagger.Binds
 import dagger.Module
 import dagger.hilt.InstallIn
+import dagger.hilt.android.components.ViewModelComponent
 
 @Module
-@InstallIn
+@InstallIn(ViewModelComponent::class)
 interface UseCaseModule {
 
     @Binds
