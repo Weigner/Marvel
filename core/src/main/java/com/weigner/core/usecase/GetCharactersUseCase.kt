92cb70cb568d79e599261db880b3e3fdbd2af3bd
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2024-01-15 19:17:24 -0300

    Revert commit class GetCharactersUseCase.kt

--- core/src/main/java/com/weigner/core/usecase/GetCharactersUseCase.kt
+++ core/src/main/java/com/weigner/core/usecase/GetCharactersUseCase.kt
@@ -5,27 +5,19 @@ import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.weigner.core.data.repository.CharactersRepository
 import com.weigner.core.domain.model.Character
-import com.weigner.core.usecase.GetCharactersUseCase.GetCharactersParams
 import com.weigner.core.usecase.base.PagingUseCase
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
-interface GetCharactersUseCase {
-    operator fun invoke(params: GetCharactersParams): Flow<PagingData<Character>>
-
-    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
-}
-
-class GetCharactersUseCaseImpl @Inject constructor(
+class GetCharactersUseCase @Inject constructor(
     private val charactersRepository: CharactersRepository
-) : PagingUseCase<GetCharactersParams, Character>(), GetCharactersUseCase {
+) : PagingUseCase<GetCharactersUseCase.GetCharactersParams, Character>() {
 
     override fun createFlowObservable(params: GetCharactersParams): Flow<PagingData<Character>> {
-        val pagingSource = charactersRepository.getCharacters(params.query)
         return Pager(config = params.pagingConfig) {
-            pagingSource
+            charactersRepository.getCharacters(params.query)
         }.flow
     }
 
-
+    data class GetCharactersParams(val query: String, val pagingConfig: PagingConfig)
 }
