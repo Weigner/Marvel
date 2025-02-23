c1528227e0117fc78a0f669caa947594f4f61484
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2025-02-23 13:13:59 -0300

    Importa lib do room e implementa entidade Favoritos

---
+++ core/src/main/java/com/weigner/core/data/DbConstants.kt
@@ -0,0 +1,8 @@
+package com.weigner.core.data
+
+object DbConstants {
+    const val FAVORITES_TABLE_NAME = "favorites"
+    const val FAVORITES_COLUMN_INFO_ID = "favorites"
+    const val FAVORITES_COLUMN_INFO_NAME = "favorites"
+    const val FAVORITES_COLUMN_INFO_IMAGE_URL = "favorites"
+}
