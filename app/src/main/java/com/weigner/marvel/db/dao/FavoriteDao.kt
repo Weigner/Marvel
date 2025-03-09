6a7b6e6aae692efb23c527f883b50abf7f8682a1
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2025-03-09 13:35:23 -0300

    Implementa DAO para os favoritos

---
+++ app/src/main/java/com/weigner/marvel/db/dao/FavoriteDao.kt
@@ -0,0 +1,23 @@
+package com.weigner.marvel.db.dao
+
+import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.weigner.core.data.DbConstants
+import com.weigner.marvel.db.entity.FavoriteEntity
+import kotlinx.coroutines.flow.Flow
+
+@Dao
+interface FavoriteDao {
+
+    @Query("SELECT * FROM ${DbConstants.FAVORITES_TABLE_NAME}")
+    suspend fun loadFavorites(): Flow<List<FavoriteEntity>>
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertFavorite(favorite: FavoriteEntity)
+
+    @Delete
+    suspend fun deleteFavorite(favorite: FavoriteEntity)
+}
