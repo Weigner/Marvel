2339d8c0b7ec479e03ec77b4723c6200d0fb6bda
Erick Weigner Freitas Cunha <erickweiner@gmail.com>
2025-09-14 22:01:14 -0300

    Implementa a ação de favoritar no DetailViewModel

--- app/src/main/java/com/weigner/marvel/presentation/detail/DetailViewModel.kt
+++ app/src/main/java/com/weigner/marvel/presentation/detail/DetailViewModel.kt
@@ -1,14 +1,17 @@
 package com.weigner.marvel.presentation.detail
 
+import androidx.annotation.DrawableRes
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.weigner.core.domain.model.Comic
 import com.weigner.core.domain.model.Event
+import com.weigner.core.usecase.AddFavoriteUseCase
 import com.weigner.core.usecase.GetCharacterCategoriesUseCase
 import com.weigner.core.usecase.base.ResultStatus
 import com.weigner.marvel.R
+import com.weigner.marvel.presentation.extentions.watchStatus
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.launch
@@ -16,66 +19,90 @@ import javax.inject.Inject
 
 @HiltViewModel
 class DetailViewModel @Inject constructor(
-    private val getCharacterCategoriesUseCase: GetCharacterCategoriesUseCase
+    private val getCharacterCategoriesUseCase: GetCharacterCategoriesUseCase,
+    private val addFavoriteUseCase: AddFavoriteUseCase
 ) : ViewModel() {
 
     private val _uiState = MutableLiveData<UiStates>()
     val uiState: LiveData<UiStates> get() = _uiState
+
+    private val _favoriteUiState = MutableLiveData<FavoriteUiStates>()
+    val favoriteUiState: LiveData<FavoriteUiStates> get() = _favoriteUiState
+
+    init {
+        _favoriteUiState.value = FavoriteUiStates.FavoriteIcon(R.drawable.ic_favorite_unchecked)
+    }
+
     fun getCharactersCategories(characterId: Int) = viewModelScope.launch {
         getCharacterCategoriesUseCase
             .invoke(GetCharacterCategoriesUseCase.GetCategoriesParams(characterId))
-            .watchStatus()
-    }
+            .watchStatus(
+                loading = {
+                    _uiState.value = UiStates.Loading
+                },
+                success = { data ->
+                    val detailParentList = mutableListOf<DetailParentVE>()
 
-    private fun Flow<ResultStatus<Pair<List<Comic>, List<Event>>>>.watchStatus() =
-        viewModelScope.launch {
-            collect { status ->
-                _uiState.value = when (status) {
-                    ResultStatus.Loading -> UiStates.Loading
-                    is ResultStatus.Success -> {
-                        val detailParentList = mutableListOf<DetailParentVE>()
-
-                        val comics = status.data.first
-                        if (comics.isNotEmpty()) {
-                            comics.map {
-                                DetailChildVE(it.id, it.imageUrl)
-                            }.also {
-                                detailParentList.add(
-                                    DetailParentVE(R.string.details_comics_category, it)
-                                )
-                            }
+                    val comics = data.first
+                    if (comics.isNotEmpty()) {
+                        comics.map {
+                            DetailChildVE(it.id, it.imageUrl)
+                        }.also {
+                            detailParentList.add(
+                                DetailParentVE(R.string.details_comics_category, it)
+                            )
                         }
-
-                        val events = status.data.second
-                        if (events.isNotEmpty()) {
-                            events.map {
-                                DetailChildVE(it.id, it.imageUrl)
-                            }.also {
-                                detailParentList.add(
-                                    DetailParentVE(R.string.details_events_category, it)
-                                )
-                            }
-                        }
-
-                        if (detailParentList.isNotEmpty()) {
-                            UiStates.Success(detailParentList)
-                        } else {
-                            UiStates.Empty
-                        }
-
-
                     }
 
-                    is ResultStatus.Error -> UiStates.Error
-                }
+                    val events = data.second
+                    if (events.isNotEmpty()) {
+                        events.map {
+                            DetailChildVE(it.id, it.imageUrl)
+                        }.also {
+                            detailParentList.add(
+                                DetailParentVE(R.string.details_events_category, it)
+                            )
+                        }
+                    }
 
-            }
+                    _uiState.value = if (detailParentList.isNotEmpty()) {
+                        UiStates.Success(detailParentList)
+                    } else {
+                        UiStates.Empty
+                    }
+                },
+                error = {
+                    _uiState.value = UiStates.Error
+                }
+            )
+    }
+
+    private fun updateFavorite(detailViewArg: DetailViewArg) = viewModelScope.launch {
+        detailViewArg.run {
+            addFavoriteUseCase.invoke(
+                AddFavoriteUseCase.Params(characterId, name, imageUrl)
+            ).watchStatus(
+                loading = {
+                    _favoriteUiState.value = FavoriteUiStates.Loading
+                },
+                success = {
+                    _favoriteUiState.value = FavoriteUiStates.FavoriteIcon(R.drawable.ic_favorite_checked)
+                },
+                error = {}
+            )
+
         }
+    }
 
     sealed class UiStates {
         object Loading : UiStates()
         data class Success(val detailParentList: List<DetailParentVE>) : UiStates()
         object Error : UiStates()
         object Empty : UiStates()
+    }
+
+    sealed class FavoriteUiStates {
+        object Loading : FavoriteUiStates()
+        class FavoriteIcon(@DrawableRes val icon: Int) : FavoriteUiStates()
     }
 }
